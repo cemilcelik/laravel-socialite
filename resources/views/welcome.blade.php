c4db0cb66da6965430021fc7ba5f85dca3dd1896
cemilcelik <cemilcelik84@gmail.com>
2017-10-08 10:22:55 +0300

    Commit from vscode test.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Socialite
                 </div>
 
                 <div class="links">
